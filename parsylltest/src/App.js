d0a4ca97de51aac20401d4c789b792e66c63e72c
Sriram MK <mksriram@umich.edu>
2024-05-04 20:35:39 -0400

    nothing makes sense here, not sure why

---
+++ parsylltest/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import { useSession, useSessionContext, useSupabaseClient } from '@supabase/auth-helpers-react';
+function App() {
+  const session = useSession(); //curent active tokens are stores inside here // tokens 
+  const supabase = useSupabaseClient(); //talking to supbase 
+
+  async function signIn() { 
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        //oauth scopes of the provider 
+        scopes: 'https://www.googleapis.com/auth/calendar'
+      }
+    }); 
+
+    if ( error ){
+      alert ("error logging in to supbasea ");
+      console.log(error); 
+    }
+  }
+
+  async function signOut() { 
+    await supabase.auth.signOut(); 
+  }
+  console.log(session); 
+  return (
+    <div className="App">
+      <div style = {{width: "400", margin: "30px auto"}}>
+        {session ? 
+        <> 
+          <h2> hey there {session.user.email} </h2>
+          <button onClick={ () => signOut()} > Sign in with Google </button>
+        </>
+       
+        :
+        <> 
+        <button onClick={ () => signIn()} > Sign in with Google </button>
+        </>
+        }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ parsylltest/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {createClient} from '@supabase/supabase-js'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+const supabaseUrl = 'https://ggonlwuwbkborocvvovp.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+    <App />
+    </SessionContextProvider >
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
